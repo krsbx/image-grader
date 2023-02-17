e0e559b70296ee1e954c0a2a8af1126a1f0e5289
krsbx <firdaus.sati51@gmail.cm>
2023-02-17 18:40:05 +0700

    chore: update models

---
+++ test/utils/constant.ts
@@ -0,0 +1,7 @@
+export const IMAGES = {
+  A: ['img-A-45.jpg', 'img-A-292.jpg'],
+  AB: ['img-AB-83.jpg', 'img-AB-186.jpg', 'img-AB-231.jpg', 'img-AB-302.jpg'],
+  B: ['img-B-4.jpg', 'img-B-115.jpg'],
+  BC: ['img-BC-1.jpg', 'img-BC-100.jpg', 'img-BC-101.jpg'],
+  E: ['img-E-10.jpg', 'img-E-103.jpg'],
+} as const;
--- test/utils/mock.ts
+++ test/utils/mock.ts
@@ -3,6 +3,8 @@ import supertest from 'supertest';
 import OpenCv from '../../src/utils/OpenCv';
 import root from '../../src/utils/root';
 import Tensorflow from '../../src/utils/Tensorflow';
+import { loadImageToBase64 } from './common';
+import { IMAGES } from './constant';
 
 export const createMockApi = async () => {
   await OpenCv.init();
@@ -20,4 +22,28 @@ export const createPostRequest = (app: Express, endpoint = '/grades') => {
     .post(endpoint)
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json');
+};
+
+export const createBulkRequest = async (app: Express) => {
+  const images = [
+    IMAGES.A[0],
+    IMAGES.A[1],
+    IMAGES.B[0],
+    IMAGES.B[1],
+    IMAGES.BC[0],
+    IMAGES.BC[1],
+    IMAGES.E[0],
+    IMAGES.E[1],
+  ];
+
+  const request: supertest.Response[] = [];
+
+  for (const [, image] of Object.entries(images))
+    request.push(
+      await createPostRequest(app).send({
+        img: await loadImageToBase64(`images/${image}`),
+      })
+    );
+
+  return request;
 };
